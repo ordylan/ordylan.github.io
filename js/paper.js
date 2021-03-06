baefde1908599b54b62604cafbb233188bcab312
ordylan <ordylan@outlook.com>
2021-03-06 21:38:23 +0800

    message

--- js/paper.js
+++ js/paper.js
@@ -1,5 +1,5 @@
 //p文章，g游戏，l栏目,pm文章描述，gm游戏描述,gi游戏图片链接描述,gu游戏跳转链接，pu文章跳转链接,pa文章，ga游戏
-//b标签，
+//b标签，a关于
 //---------------l栏目---------------------------
 document.getElementById("l1").innerHTML="公告栏";
 document.getElementById("l2").innerHTML="文章推荐";
@@ -62,9 +62,9 @@ document.getElementById("b4").href="../ordylan/ordylan_game/index.html";
 document.getElementById("b5").href="../ordylan/ordylan_music/index.html";
 //document.getElementById("b6").href="#";
 document.getElementById("b7").href="../index.html";
-
-
-
+//---------------a关于------------------------
+document.getElementById("a").innerHTML="关于我";
+document.getElementById("a").href="../ordylan/about/index.html"; 
 //---------------通知在下面---------------------------------------------------------------------------
 
 
@@ -73,33 +73,5 @@ document.getElementById("b7").href="../index.html";
 //t图片+miaoshu，w文字,
 //---------------通知旧to新---------------------------
 
-//---------------通知1---------------------------
-document.getElementById("w1").innerHTML="欢迎来到橙子网!";
-document.getElementById("w1").href="#"; 
-//---------------通知2---------------------------
-document.getElementById("t2").alt="橙子电影院欢迎您!";
-document.getElementById("t2").src="image/cinema.png";
-document.getElementById("w2").innerHTML="橙子电影院来啦!点我进入!";
-document.getElementById("w2").href="../ordylan/ordylan_cinema/index.html"; 
-//---------------通知3---------------------------
-document.getElementById("w3").innerHTML="主页大改版，快来看看吧~";
-document.getElementById("w3").href="index.html"; 
-//---------------通知4---------------------------
-document.getElementById("t4").alt="橙鸭快乐跳2来啦";
-document.getElementById("t4").src="image/20200825.svg";
-document.getElementById("w4").innerHTML="橙鸭快乐跳2来啦，点我去玩&gt;&gt;";
-document.getElementById("w4").href="../ordylan/ordylan_game/games/o_jump_2/index.html"; 
-//---------------通知5---------------------------
-document.getElementById("t5").alt="橙子网一周年啦!";
-document.getElementById("t5").src="image/logo1year.svg";
-document.getElementById("w5").innerHTML="橙子网一周年啦!";
-document.getElementById("w5").href="../ordylan/ordylan_article/articles/2020.9.25_1/index.html"; 
-//---------------通知6---------------------------
-document.getElementById("t6").alt="橙子网4.0庆典已经到来";
-document.getElementById("t6").src="huodong/qingdian/youwanquan.png";
-document.getElementById("w6").innerHTML="橙子网4.0庆典已经到来,快来庆典游玩吧!";
-document.getElementById("w6").href="../ordylan/ordylan_article/articles/2020.10.18_1/index.html"; 
-//---------------通知7---------------------------
-document.getElementById("w7").innerHTML="每页一句来啦,每次加载文字都会变哦~";
-//---------------通知8---------------------------
-document.getElementById("w8").innerHTML="[置项通知]重要通知:<br>橙子网主页大升级,请务必使用可以浏览js的浏览器,如果你可以看见这句话,说明你的浏览器支持js。橙子电影院因为版权原因下线。未来会换成别的。";
+document.getElementById("w1").innerHTML="[置项通知]<br>欢迎来到内服!!!";
+document.getElementById("w2").innerHTML="认真代码!!!!!!!!!!凑字数凑字数凑字数凑字数凑字数凑字数凑字数";
