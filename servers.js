edfab3ec41c1495d64a2d9086b9c80ba3d56a91d
ordylan <56828391+ordylan@users.noreply.github.com>
2021-05-03 16:18:09 +0800

    Add files via upload

--- servers.js
+++ servers.js
@@ -12,14 +12,21 @@ document.getElementById("s6").innerHTML="-内服-<br>优若雪山<br>(IIS)";
 document.getElementById("s7").innerHTML="-空闲-<br>电脑集市<br>(主站电脑版)";
 document.getElementById("s8").innerHTML="-下载-<br>注释沼泽<br>(橙子浏览器下载)";
 
-document.getElementById("s1").href="servers/gh.html";
-document.getElementById("s2").href="mainpage/admin/l.html";
-document.getElementById("s3").href="servers/gh.html";
-document.getElementById("s4").href="servers/od.html";
-document.getElementById("s5").href="servers/ge.html";
-document.getElementById("s6").href="servers/iis.html";
-document.getElementById("s7").href="servers/win.html";
-document.getElementById("s8").href="servers/b.html";
+document.getElementById("s1").href="javascript:void(0)";
+
+document.getElementById("s2").href="javascript:void(0)";
+
+document.getElementById("s3").href="javascript:void(0)";
+
+document.getElementById("s4").href="javascript:void(0)";
+
+document.getElementById("s5").href="javascript:void(0)";
+
+document.getElementById("s6").href="javascript:void(0)";
+
+document.getElementById("s7").href="javascript:void(0)";
+
+document.getElementById("s8").href="javascript:void(0)";
 
 //活动
 document.getElementById("hdimg").alt="活动的图片描述呵呵哈哈哈.";
@@ -27,3 +34,14 @@ document.getElementById("hdimg").src="#";
 document.getElementById("hdurl").href="#"; 
 document.getElementById("hdurl").innerHTML="活动的qian wang文字字."; 
 document.getElementById("hdms").innerHTML="活动的文字字字字字字."; 
+
+
+document.getElementById("stz1").innerHTML="欢迎来到网页山谷服务器(繁忙)!"; 
+document.getElementById("stz2").innerHTML="阿得梦山脉"; 
+document.getElementById("stz4").innerHTML="代码小镇服务器(错误)!"; 
+document.getElementById("stz5").innerHTML="元素王国服务器正在维护!"; 
+document.getElementById("stz6").innerHTML="很抱歉，你目前无法访问此服务器"; 
+document.getElementById("stz7").innerHTML="欢迎来到电脑集市服务器(空闲)!"; 
+document.getElementById("stz8").innerHTML="欢迎来到注释沼泽服务器(橙子浏览器下载)![错误!找不到文件。]"; 
+document.getElementById("stz3").innerHTML="欢迎来到网页山谷服务器(繁忙)!"; 
+
